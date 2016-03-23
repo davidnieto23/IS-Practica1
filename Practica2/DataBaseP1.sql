60a5c859a8d92fdad8a90e94e363c4b82701a908
David Alejandro Nieto <davidalexito23@gmail.com>
2016-03-22 23:18:29 -0600

    agrego Readme.txt a la practica

---
+++ Practica2/DataBaseP1.sql
@@ -0,0 +1,28 @@
+﻿-- Database: "IngSoft"
+-- Practica 1
+
+-- DROP DATABASE "IngSoft";
+
+CREATE DATABASE "IngSoft"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'C'
+       LC_CTYPE = 'C'
+       CONNECTION LIMIT = -1;
+
+--Creo tabla para el Usuario con sus atributos
+CREATE TABLE Usuario( 
+  nidUsuario serial PRIMARY KEY,
+  nombre VARCHAR(40) NOT NULL,
+  correo VARCHAR(128) NOT NULL,
+  contraseña VARCHAR(10) NOT NULL
+);
+
+--Creo tabla para el RegistroUsuario
+CREATE TABLE RegistroUsuario(
+  numeroDeSesiones INTEGER,
+  nidUsuario serial REFERENCES Usuario(nidUsuario),
+  idregistro serial PRIMARY KEY
+ );
+
---
+++ Practica2/README.txt
@@ -0,0 +1,11 @@
+Nieto Andrade David Alejandro - 310169846
+
+Instalación :
+
+	- Contiene un Script para la creacion de la base de datos,
+		recomiendo que se ejecute primero el "create database",
+		luego las dos tablas.
+
+	- Cree las interfaces necesarias para la practica, pero
+		no funciona correctamente la aplicación, tuve algunos 
+		problemas con hibernate.
